a7a72bccf11b43743eaee3c97e0b5bb25d5f86a4
vcbdh <134359548+vcbdh@users.noreply.github.com>
2023-05-23 05:31:16 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #请修改为自己的UUID
-export UUID=e4c40e74-8352-4bba-a36c-10349334eba4
+export UUID=2c789fc1-66ef-4ac5-a924-7bb797dc7770
 
 #请修改为自己设置的伪装站，不要带https://
-export ProxySite=techtanker.com
+export ProxySite=albshara.com
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
